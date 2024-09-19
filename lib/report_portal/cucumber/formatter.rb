91daecdf822f063e82d5a062278f9b655b9ffd0a
yan_ramanovich@epam.com <yan_ramanovich@epam.com>
2024-09-19 18:37:58 +0200

    Fix

--- lib/report_portal/cucumber/formatter.rb
+++ lib/report_portal/cucumber/formatter.rb
@@ -57,6 +57,7 @@ module ReportPortal
         if existing_suite_node
           @parent_item_node = existing_suite_node
         else
+          binding.pry
           unless @parent_item_node.content.is_a?(ReportPortal::TestItem)
             ReportPortal.finish_suite(@parent_item_node)
           end
