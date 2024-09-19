964ce28127615dc20bcc30e5e69e4719e702ac58
yan_ramanovich@epam.com <yan_ramanovich@epam.com>
2024-09-19 17:06:07 +0200

    Fix

--- lib/report_portal/cucumber/formatter.rb
+++ lib/report_portal/cucumber/formatter.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
-require 'irb'
 require 'cucumber'
 require 'cucumber/formatter/pretty'
 require 'securerandom'
 require 'tree'
+require 'irb'
 require_relative '../../reportportal'
-require 'fileutils'
 
 module ReportPortal
   module Cucumber
-    # Formatter for Cucumber
     class Formatter < ::Cucumber::Formatter::Pretty
       MAX_DESCRIPTION_LENGTH = 255
       MIN_DESCRIPTION_LENGTH = 3
@@ -18,21 +16,19 @@ module ReportPortal
       def initialize(config)
         super(config)
 
-        @root_node = Tree::TreeNode.new(SecureRandom.hex)
-        @parent_item_node = @root_node
-        @last_used_time = 0
+        @root_node = Tree::TreeNode.new(SecureRandom.hex, "Launch")
+        @parent_item_node = @root_node # Родительским узлом по умолчанию будет корень (лаунч)
       end
 
       def on_gherkin_source_read(event)
         super(event)
-        ReportPortal.start_launch(description: nil)
+        ReportPortal.start_launch(description: 'Test Launch')
       end
 
       def on_test_run_finished(event)
         super(event)
         ReportPortal.finish_launch
       end
-
 
       def on_test_case_started(event)
         super(event)
@@ -42,28 +38,36 @@ module ReportPortal
       private
 
       def feature_started(feature:)
+        binding.irb
         feature_name = feature.name
         feature_tags = feature.tags
         tag_names = feature_tags.map(&:name)
 
         if feature_name.size < MIN_DESCRIPTION_LENGTH
-          p "Group description should be at least #{MIN_DESCRIPTION_LENGTH} characters ('group_notification': #{feature.inspect})"
+          p "Описание группы должно содержать минимум #{MIN_DESCRIPTION_LENGTH} символов ('group_notification': #{feature.inspect})"
           return
         end
 
-        item = ReportPortal::TestItem.new(name: feature_name[0..MAX_DESCRIPTION_LENGTH - 1],
-                                          type: :SUITE,
-                                          start_time: ReportPortal.now,
-                                          description: feature_name,
-                                          tags: tag_names)
+        existing_suite_node = @root_node.breadth_each.find { |node| node.content == feature_name }
 
-        group_node = Tree::TreeNode.new(SecureRandom.hex, item)
-        if group_node.nil?
-          p "Group node is nil for item #{item.inspect}"
+        if existing_suite_node
+          p "Сьют с именем '#{feature_name}' уже существует, добавляем тесты в существующий сьют."
+          @parent_item_node = existing_suite_node
         else
-          @parent_item_node << group_node unless @parent_item_node.nil? # make @current_group_node parent of group_node
-          @parent_item_node = group_node
-          group_node.content.id = ReportPortal.start_item(group_node)
+          suite_item = ReportPortal::TestItem.new(name: feature_name[0..MAX_DESCRIPTION_LENGTH - 1],
+                                                  type: :SUITE,
+                                                  start_time: ReportPortal.now,
+                                                  description: feature_name,
+                                                  tags: tag_names)
+
+          suite_node = Tree::TreeNode.new(SecureRandom.hex, feature_name)
+          if suite_node.nil?
+            p "Сьют не может быть создан: #{suite_item.inspect}"
+          else
+            @root_node << suite_node
+            @parent_item_node = suite_node
+            suite_node.content.id = ReportPortal.start_item(suite_node)
+          end
         end
       end
     end
