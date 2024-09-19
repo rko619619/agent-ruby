3c7f008cffc0d8b2d7cb9e24a198d60be9f9eccc
yan_ramanovich@epam.com <yan_ramanovich@epam.com>
2024-09-19 13:39:13 +0200

    Fix

--- lib/report_portal/cucumber/formatter.rb
+++ lib/report_portal/cucumber/formatter.rb
@@ -3,23 +3,26 @@
 require 'irb'
 require 'cucumber'
 require 'cucumber/formatter/pretty'
+binding.irb
 require 'reportportal'
 require 'fileutils'
 
 module ReportPortal
   module Cucumber
+    binding.pry
     # Formatter for Cucumber
-    class Formatter < ::Cucumber::Formatter::Pretty
+    class Formatter < AAA
       def initialize(config)
         super(config)
       end
 
-      def gherkin_source_read(event)
+      def on_gherkin_source_read(event)
+        binding.irb
         super(event)
         ReportPortal.start_launch(description: '123')
       end
 
-      def test_case_started(event)
+      def on_test_case_started(event)
         binding.irb
         puts 123123123123123123
         super(event)
