873f58bf252ef089cf2d0e4321abc336051e86d0
yan_ramanovich@epam.com <yan_ramanovich@epam.com>
2024-09-27 15:52:29 +0200

    Refactor cucumber/formatter

--- lib/report_portal/cucumber/formatter.rb
+++ lib/report_portal/cucumber/formatter.rb
@@ -49,7 +49,7 @@ module ReportPortal
       end
 
       def handle_test_case_started(event)
-        @cucumber_helper.feature_suite_started(feature: event.gherkin_document.feature)
+        @cucumber_helper.feature_suite_started(feature: gherkin_document.feature)
         @cucumber_helper.test_case_started(test_case: event.test_case)
       end
 
