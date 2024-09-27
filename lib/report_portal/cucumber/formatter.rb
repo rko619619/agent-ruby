644906e6c03b4f0fd1b659e7bbb0f926dcf4e472
yan_ramanovich@epam.com <yan_ramanovich@epam.com>
2024-09-27 15:46:29 +0200

    Refactor cucumber/formatter

--- lib/report_portal/cucumber/formatter.rb
+++ lib/report_portal/cucumber/formatter.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 require 'cucumber/formatter/pretty'
-require 'irb'
 require_relative '../../reportportal'
 require_relative 'cucumber_helper'
 
 module ReportPortal
   module Cucumber
-    # report portal formatter with pretty for cucumber
+    # Report Portal formatter with Pretty for Cucumber
     class Formatter < ::Cucumber::Formatter::Pretty
       def initialize(config)
         super(config)
@@ -16,32 +15,49 @@ module ReportPortal
 
       def bind_events(config)
         super(config)
-        config.on_event :test_run_started, &method(:on_test_run_started)
-      end
-
-      def on_test_run_started(event)
-        @cucumber_helper.start_launch
+        config.on_event :test_run_started, &method(:handle_test_run_started)
       end
 
       def on_test_run_finished(event)
         super(event)
-        @cucumber_helper.finish_suite
-        @cucumber_helper.finish_launch
+        finalize_launch
       end
 
       def on_test_case_started(event)
-        super(event)
-        @cucumber_helper.feature_suite_started(feature: gherkin_document.feature)
-        @cucumber_helper.test_case_started(test_case: event.test_case)
+        handle_test_case_started(event)
       end
 
       def on_test_case_finished(event)
         super(event)
-        @cucumber_helper.test_case_finished(test_case_result: event.result)
+        handle_test_case_finished(event)
       end
 
       def on_test_step_finished(event)
         super(event)
+        handle_test_step_finished(event)
+      end
+
+      private
+
+      def handle_test_run_started(event)
+        @cucumber_helper.start_launch
+      end
+
+      def finalize_launch
+        @cucumber_helper.finish_suite
+        @cucumber_helper.finish_launch
+      end
+
+      def handle_test_case_started(event)
+        @cucumber_helper.feature_suite_started(feature: gherkin_document.feature)
+        @cucumber_helper.test_case_started(test_case: event.test_case)
+      end
+
+      def handle_test_case_finished(event)
+        @cucumber_helper.test_case_finished(test_case_result: event.result)
+      end
+
+      def handle_test_step_finished(event)
         @cucumber_helper.test_step_finished(test_step: event.test_step, test_step_result: event.result)
       end
     end
