f4e280ed57c2f04b9683391a3fb604cf72542875
yan_ramanovich@epam.com <yan_ramanovich@epam.com>
2024-09-27 18:50:25 +0200

    Refactor models/test_item.rb

--- lib/report_portal/models/test_item.rb
+++ lib/report_portal/models/test_item.rb
@@ -5,18 +5,20 @@ module ReportPortal
   class TestItem
     attr_accessor :id, :closed, :launch_id, :unique_id, :name, :description, :type, :parameters, :tags, :status, :start_time
 
+    # Initializes the test item with the provided options
     def initialize(options = {})
+      assign_attributes(options)
+    end
+
+    private
+
+    # Assigns attributes from the provided options hash
+    def assign_attributes(options)
       options = options.transform_keys(&:to_sym)
-      @launch_id = options[:launch_id]
-      @unique_id = options[:unique_id]
       @name = options[:name]
-      @description = options[:description]
       @type = options[:type]
-      @parameters = options[:parameters]
-      @tags = options[:tags]
-      @status = options[:status]
       @start_time = options[:start_time]
-      @id = options[:id]
+      @description = options[:description]
       @closed = options[:closed]
     end
   end
