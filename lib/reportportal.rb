a1239b62236e00bf182435649c1796161cf3661c
yan_ramanovich@epam.com <yan_ramanovich@epam.com>
2024-09-27 14:13:47 +0200

    Refactor

--- lib/reportportal.rb
+++ lib/reportportal.rb
@@ -23,6 +23,7 @@ module ReportPortal
     attr_accessor :launch_id, :current_scenario, :start_time, :name, :type, :description, :tags
 
     def now
+      binding.irb
       (current_time.to_f * 1000).to_i
     end
 
