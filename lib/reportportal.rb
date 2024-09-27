d2b036c71012e9b1faaaa42325ebb6cf2629e93b
yan_ramanovich@epam.com <yan_ramanovich@epam.com>
2024-09-27 18:13:05 +0200

    Remove unused files for cucumber

--- lib/reportportal.rb
+++ lib/reportportal.rb
@@ -10,11 +10,7 @@ require 'tempfile'
 require 'uri'
 require 'irb'
 
-require_relative 'report_portal/event_bus'
-require_relative 'report_portal/models/item_search_options'
-require_relative 'report_portal/models/test_item'
-require_relative 'report_portal/settings'
-require_relative 'report_portal/'
+require_relative 'settings'
 
 module ReportPortal
   LOG_LEVELS = { error: 'ERROR', warn: 'WARN', info: 'INFO', debug: 'DEBUG', trace: 'TRACE', fatal: 'FATAL', unknown: 'UNKNOWN' }.freeze
