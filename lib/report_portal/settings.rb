5ff56c24a95c74a5a49c719c4d8fa7e4c8ca3c9a
yan_ramanovich@epam.com <yan_ramanovich@epam.com>
2024-09-30 10:17:22 +0200

    Refactor formatter service

--- lib/report_portal/settings.rb
+++ lib/report_portal/settings.rb
@@ -26,7 +26,6 @@ module ReportPortal
         'launch_id' => false,
         'file_with_launch_id' => false,
         'logLaunchLink' => false,
-        'formatter_mode' => true,
         'cucumber_formatter' => false
       }
 
