b76d4d5dcb619a367812307f119f21b73fbdac4d
yan_ramanovich@epam.com <yan_ramanovich@epam.com>
2024-09-30 10:15:46 +0200

    Refactor formatter service

--- lib/report_portal/custom_logger.rb
+++ lib/report_portal/custom_logger.rb
@@ -6,7 +6,6 @@ module ReportPortal
   class CustomLogger < Logger
     def initialize
       super($stdout)
-      @level = fetch_log_level
       @formatter = proc do |severity, datetime, _progname, msg|
         "#{datetime.strftime('%Y-%m-%d %H:%M:%S')} - #{severity.ljust(5)}: #{msg}\n"
       end
